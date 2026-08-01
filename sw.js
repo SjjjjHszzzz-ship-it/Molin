8db736393d969a860c093b2e02c6a47d294922d5
Molin <molin@workbench.com>
2026-08-01 11:07:14 +0800

    🦋 Molin专属工作台 PWA 部署

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'molin-workbench-v3';
+const ASSETS = [
+  './喵喵工作台.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(response => {
+      if (response && response.status === 200) {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone)).catch(()=>{});
+      }
+      return response;
+    }).catch(() => {
+      return caches.match(e.request).then(cached => cached || Response.error());
+    })
+  );
+});
